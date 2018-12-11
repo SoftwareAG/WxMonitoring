789da139943212de48385ded3fda961a97b217d3
Puneet Arora <puneet.arora@softwareag.com>
2018-12-11 12:06:04 +0100

    temporary logstash exception process bug fix

--- assets/IS/Packages/WxMonitoring/pub/elk/logstash/startLogstash.cmd
+++ assets/IS/Packages/WxMonitoring/pub/elk/logstash/startLogstash.cmd
@@ -2,13 +2,13 @@
 
 setlocal
 
-set SAG_HOME=c:\SoftwareAG
-set LOGSTASH_HOME=C:\work\local\logstash-6.2.3
+set SAG_HOME=C:\SoftwareAG910
+set LOGSTASH_HOME=C:\SoftwareAG910\logstash-6.2.2
 set LOGSTASH_PORT=5044
 set LOGSTASH_HTTP_IMPORT_PORT=5045
 set ELASTIC_SEARCH_ADDRESS=localhost:9200
 set LOGSTASH_CONFIG=%~dp0WxMonitoring.conf
 
-%LOGSTASH_HOME%\bin\logstash.bat --pipeline.id WxMonitoring -w 1 -f %LOGSTASH_CONFIG%s
+%LOGSTASH_HOME%\bin\logstash.bat --pipeline.id WxMonitoring -w 1 -f %LOGSTASH_CONFIG%
 
 endlocal	
