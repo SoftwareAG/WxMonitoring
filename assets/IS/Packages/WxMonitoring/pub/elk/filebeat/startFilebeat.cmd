865af74db82446e10a19d6246b213de83d375621
Puneet Arora <puneet.arora@softwareag.com>
2019-06-13 14:37:07 +0200

    upgrade filebeat 6.2.2 to 7.1.1

--- assets/IS/Packages/WxMonitoring/pub/elk/filebeat/startFilebeat.cmd
+++ assets/IS/Packages/WxMonitoring/pub/elk/filebeat/startFilebeat.cmd
@@ -4,12 +4,12 @@ setlocal
 
 rem **** Please edit these settings ****
 
-set SAG_HOME=C:\SAG103
-set FILEBEAT_HOME=C:\work\local\filebeat-7.1.1-windows-x86_64
+set SAG_HOME=C:\SoftwareAG
+set FILEBEAT_HOME=C:\SoftwareAG910\elastic-stack\filebeat\filebeat-7.1.1
 set SERVER_ID=dev1
 set LOGSTASH_HOST=localhost
 set LOGSTASH_PORT=5044
-set SERVER_LOGFILE_PATH=%SAG_HOME%\IntegrationServer\instances\default\logs\server.log
+set SERVER_LOGFILE_PATH=C:\LOG_COLLECTION\sample_server_logs.log
 set WRAPPER_LOGFILE_PATH=%SAG_HOME%\profiles\*\*\wrapper.log
 
 rem **** END ****
